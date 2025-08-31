b61b967c9d030cc612d3c13484f9937424078ffa
Diogo Oliveira <dluks82@gmail.com>
2025-08-31 00:23:51 -0300

    fix: otimização da configuração de TypeScript e ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: ['./tsconfig.json', './tsconfig.test.json'],
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     '^.+\\.ts$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.json',
+        tsconfig: 'tsconfig.test.json',
       },
     ],
   },
