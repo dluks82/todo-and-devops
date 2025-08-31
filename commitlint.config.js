50f6af47c36439feb1c83d3567f87a4642ea92f8
Diogo Oliveira <dluks82@gmail.com>
2025-08-31 00:47:02 -0300

    build: atualizar configuração do eslint e corrigir husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [0, 'never'], // Desativando a regra de case para o subject
     'body-max-line-length': [0, 'always', 100],
   },
 };
---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const path = require('path');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: {
+    extends: ['eslint:recommended'],
+  },
+});
+
+module.exports = [
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier'
+  ),
+  {
+    files: ['**/*.ts'],
+    ignores: [
+      'dist/**', 
+      'node_modules/**', 
+      'coverage/**',
+      '*.config.js',
+      'commitlint.config.js',
+      'eslint.config.js',
+      '.husky/**',
+      'jest.config.js',
+    ],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.test.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': ['warn', {
+        'allowExpressions': true,
+        'allowTypedFunctionExpressions': true
+      }],
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }]
+    },
+  },
+];
